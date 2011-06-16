4af93531b0b7eee1970325d0203abda4b9b2d682
Chris Tosswill <ctosswill@engineyard.com>
2011-06-16 10:49:39 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefineEy::Application.config.secret_token = '7ea792c7b85bffe84b5ffa7aa735abb1aa379e8f4e0be68002108cb2e699fd683e514555b42d67242593acfa8827723f636d7d47504ecb930600077a2b4c8d7f'
